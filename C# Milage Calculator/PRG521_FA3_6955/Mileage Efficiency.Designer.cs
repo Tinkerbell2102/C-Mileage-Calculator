5fe2ac93de67b013c121f41c3b9ad75f7ee87dbe
Laikin Barnard <66776381+Tinkerbell2102@users.noreply.github.com>
2022-01-11 20:55:15 +0200

    Calculator

---
+++ C# Milage Calculator/PRG521_FA3_6955/Mileage Efficiency.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace PRG521_FA3_6955
+{
+    partial class MEfficiency
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtEfficiency = new System.Windows.Forms.TextBox();
+            this.txtGallons = new System.Windows.Forms.TextBox();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnCalculate);
+            this.panel1.Controls.Add(this.txtEfficiency);
+            this.panel1.Controls.Add(this.txtGallons);
+            this.panel1.Controls.Add(this.txtMiles);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(298, 225);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(22, 179);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtEfficiency
+            // 
+            this.txtEfficiency.Location = new System.Drawing.Point(167, 135);
+            this.txtEfficiency.Name = "txtEfficiency";
+            this.txtEfficiency.ReadOnly = true;
+            this.txtEfficiency.Size = new System.Drawing.Size(100, 23);
+            this.txtEfficiency.TabIndex = 6;
+            // 
+            // txtGallons
+            // 
+            this.txtGallons.Location = new System.Drawing.Point(140, 92);
+            this.txtGallons.Name = "txtGallons";
+            this.txtGallons.Size = new System.Drawing.Size(127, 23);
+            this.txtGallons.TabIndex = 5;
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(103, 55);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(164, 23);
+            this.txtMiles.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Efficiency (Miles/Gallon):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gallons of gas used:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Miles Driven:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(11, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mileage Efficiency Calculator";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(192, 179);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // MEfficiency
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(326, 249);
+            this.Controls.Add(this.panel1);
+            this.Name = "MEfficiency";
+            this.Text = "Mileage Efficiency";
+            this.Load += new System.EventHandler(this.MEfficiency_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtEfficiency;
+        private System.Windows.Forms.TextBox txtGallons;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ C# Milage Calculator/PRG521_FA3_6955/Mileage Efficiency.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG521_FA3_6955
+{
+    public partial class MEfficiency : Form
+    {
+        public MEfficiency()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal decMiles =
+                    Convert.ToDecimal(txtMiles.Text);
+                decimal decGallons =
+                    Convert.ToDecimal(txtGallons.Text);
+                decimal decEfficiency = decMiles / decGallons;
+                txtEfficiency.Text =
+                    String.Format("{0:n}", decEfficiency);
+
+                using (StreamWriter writer = new StreamWriter("d:\\tmp\\MilesperGallon.txt", true))
+                {
+                    writer.WriteLine(DateTime.Now);
+                    writer.WriteLine("Miles(" + decMiles + ") / " + "Gallons(" + decGallons + ") =  Miles Per Hour(" + decEfficiency + ")");
+                    writer.WriteLine("");
+                }
+            }
+
+            catch (FormatException fe)
+            {
+                string msg = String.Format(
+                     "Message: {0} Please enter a number value!\n",
+                     fe.Message);
+                MessageBox.Show(msg.ToString());
+
+                using (StreamWriter writer = new StreamWriter("d:\\tmp\\Exceptionfile.txt", true))
+                {
+                    writer.WriteLine(DateTime.Now);
+                    writer.WriteLine(fe);
+                    writer.WriteLine("");
+                }
+            }
+            catch (DivideByZeroException dbze)
+            {
+                string msg = String.Format(
+                    "Message: {0}Cannot devide by zero!\n",
+                    dbze.Message);
+                    MessageBox.Show(
+                    msg.ToString());
+
+                using (StreamWriter writer = new StreamWriter("d:\\tmp\\Exceptionfile.txt", true))
+                {
+                    writer.WriteLine(DateTime.Now);
+                    writer.WriteLine(dbze);
+                    writer.WriteLine("");
+                }
+            }
+            catch (Exception ex)
+            {
+                string msg = String.Format(
+                    "Message: {0}Please enter a smaller value\n",
+                    ex.Message);
+                    MessageBox.Show(msg.ToString());
+
+                using (StreamWriter writer = new StreamWriter("d:\\tmp\\Exceptionfile.txt", true))
+                {
+                    writer.WriteLine(DateTime.Now);
+                    writer.WriteLine(ex);
+                    writer.WriteLine("");
+                }
+            }
+            finally
+            {
+                txtMiles.Focus();
+            }
+        }
+        private void MEfficiency_Load(object sender, EventArgs e)
+        {
+            txtMiles.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMiles.Clear();
+            txtGallons.Clear();
+            txtEfficiency.Clear();
+            txtMiles.Focus();
+        }
+    } 
+}
